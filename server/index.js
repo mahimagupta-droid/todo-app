782d6c8177cdb5dcc6d3a6b70e976e5d028d2d4a
Mahima Gupta <guptamahima583@gmail.com>
2026-09-11 23:44:36 +0530

    crud complete

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
-import app from './app.js';
+import app from "./app.js";
+import 'dotenv/config.js'
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("server running on port 3000");
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(
+        "server running on port 3000 and database connected successfully",
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,44 +1,54 @@
+import TodoModel from './../models/Todo.js'
 import express from "express";
 const router = express.Router();
 
-const todos = [
-  {
-    id: 1,
-    title: "find internship",
-    status: "completed",
-  },
-];
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const todos = await TodoModel.find();
   res.send(todos);
 });
 
-router.post('/', (req, res) => {
-    const {title, status} = req.body;
-    const todo = {
-        id: todos.length + 1,
-        title: title,
-        status: status
-    };
-    todos.push(todo);
-    res.send(todo);
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const todo = await TodoModel.findById(id)
+  if(!todo) return res.status(404).json({
+    message: "Id not found"
+  });
+  res.json(todo);
 })
 
-router.put('/:id', (req, res) => {
-  const id = Number(req.params.id);
+router.post('/', async (req, res) => {
+    const {title, status} = req.body;
+    const newTodo = await TodoModel.create({
+      title, 
+      status
+    })
+    res.json(newTodo);
+})
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
   const {title, status} = req.body;
-  const todoId = todos.findIndex((todo) => todo.id === id);
-  if(todoId == -1) {
-    return res.status(404).json({
-      message: "Id not found"
-    });
-  }
-  todos[todoId] = {
-    id: id,
-    title: title,
-    status: status
-  }
-  res.json(todos);
+  const updatedTodo = await TodoModel.findByIdAndUpdate(
+    id,
+    {
+      title, 
+      status
+    }, 
+    {new: true}
+  )
+  if(!updatedTodo) return res.status(404).json({
+    message: "Todo not updated"
+  })
+  return res.json(updatedTodo);
+})
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  const deletedTodo = await TodoModel.findByIdAndDelete(id)
+  if(!deletedTodo) return res.status(404).json({
+    message: "Todo not deleted"
+  })
+  return res.json(deletedTodo);
 })
 
 export default router;
